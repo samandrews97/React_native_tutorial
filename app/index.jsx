315b592311163e5d7abc5946bda65328a9a220f1
samandrews97 <sam@asandrews.co.uk>
2026-02-19 16:05:17 +0000

    Completed Tutorial 2

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import Logo from '../assets/img/logo_light.png'
 
 const Home = () => {
     return (
-        <View>
-            <Text>Home</Text>
+        // Style using a StyleSheet
+        <View style={styles.container}> 
+            <Image source={Logo} style={styles.img}/>
+            <Text style={styles.title}>
+                The Number 1
+            </Text> 
+
+            {/* In-line Style */}
+            <Text style={{marginTop: 10, marginBottom: 30}}>
+                Reading List App
+            </Text> 
+
         </View>
     )
 }
 
 export default Home
 
-const styles = StyleSheet.create()
+const styles = StyleSheet.create({
+    //Style for all objects
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    // Style for title text
+    title: { 
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+
+    // Style for light logo
+    img: {
+        marginVertical: 20,
+
+    },
+})
